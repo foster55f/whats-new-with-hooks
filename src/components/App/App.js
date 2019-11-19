c858d4594128024e4c7c41b41f51f782f34b8f61
“Foster <fosterataylor@yahoo.com>
2019-11-19 14:18:43 -0700

    Render news articles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
 import './App.css';
+import NewsContainer from './../NewsContainer/NewsContainer'
+
 
 class App extends Component {
   constructor() {
@@ -12,9 +14,9 @@ class App extends Component {
 
   render () {
     return (
-      <div className="app">
-        YOUR CODE GOES HERE!
-      </div>
+      <main className="App">
+        <NewsContainer articles = {this.state.local}/>
+      </main>
     );
   }
 }
--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,30 @@
 import React from 'react';
-import './NewsContainer.css'
+import './NewsContainer.css';
+import NewsArticle from './../NewsArticle/NewsArticle';
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+
+const NewsContainer = (props) => {
+    const { articles } = props
+
+    const newsArticles = articles.map(article => {
+        return (
+            <NewsArticle
+                key={article.id}
+                id={article.id}
+                title={article.headline}
+                image={article.img}
+                description={article.description}
+                url={article.url}
+         
+            />
+        )
+    })
+    return (
+        <div className='news-Container'>
+           {newsArticles}
+            </div>
+    )
+}
+
 
 export default NewsContainer;
