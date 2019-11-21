641b76efad2ec209e1be36650b96f65363c69dcf
“Foster <fosterataylor@yahoo.com>
2019-11-21 08:19:19 -0700

    Add initial testing files and structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,13 +12,7 @@ class App extends Component {
     super();
     this.state = {
       "currentNews": local,
-      'data': null
-      // {
-      //   local, entertainment,
-      //   health: health,
-      //   technology,
-      //   science,
-      // }   
+      'data': null  
     }
   }
 
@@ -33,17 +27,10 @@ class App extends Component {
     this.setState({currentNews:this.state.data[data]})
   }
 
-  // filterAllNews = (type) => {
-  //   this.setState({ currentNews:this.state.data[type]})
-  // }
-
   filterSearch = (search) => {
-    console.log(search)
-    console.log(this.state.data.local)
     const searchResults = this.state.data.health.filter(article => {
       return article.headline.toLowerCase().includes(search.toLowerCase())|| article.description.toLowerCase().includes(search.toLowerCase())
     })
-    console.log(searchResults)
     this.setState({ currentNews: searchResults }) 
   }
 
