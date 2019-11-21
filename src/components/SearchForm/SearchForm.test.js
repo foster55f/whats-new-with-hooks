69999288543b301345d5bea5871a050b6e9401e2
“Foster <fosterataylor@yahoo.com>
2019-11-21 16:57:13 -0700

    Add search testing

---
+++ src/components/SearchForm/SearchForm.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SearchForm from './SearchForm';
+import { isTSAnyKeyword } from '@babel/types';
+
+describe('SearchForm', () => {
+    it('should match the snapshot', () => {
+        const filterSearchMock = jest.fn()
+        const wrapper = shallow(<SearchForm search={filterSearchMock} />)
+        
+        expect(wrapper).toMatchSnapshot();
+    })
+
+    it('should update state when search is inputted', () => {
+        const filterSearchMock = jest.fn()
+        const wrapper = shallow(<SearchForm search={filterSearchMock} />)
+
+        const searchTime = {
+            target: {
+                value: 'spider-man'
+            }
+        }
+        
+        wrapper.instance().handleChange(searchTime);
+        expect(wrapper.state('searchField')).toEqual(searchTime.target.value)
+    })
+
+    it('should update state when search is inputted', () => {
+        const filterSearchMock = jest.fn()
+        const wrapper = shallow(<SearchForm search={filterSearchMock} />)
+
+        const searchTime = {
+            target: {
+                value: 'spider-man'
+            }
+        }
+        
+        wrapper.instance().handleChange(searchTime);
+        expect(wrapper.state('searchField')).toEqual(searchTime.target.value)
+    })
+})
